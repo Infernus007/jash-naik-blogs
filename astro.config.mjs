7bf55357063f3a9e4b75efc110de54df11de67c5
Infernus007 <100568769+Infernus007@users.noreply.github.com>
2024-10-12 18:34:25 +0530

    chore : change build format

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,31 +6,23 @@ import tailwind from "@astrojs/tailwind";
 import rehypePrettyCode from "rehype-pretty-code";
 import pagefind from "astro-pagefind";
 
-
 /** @type {import('rehype-pretty-code').Options} */
 const options = {
 	// See Options section below.
-	theme: 
-	{
-	dark : JSON.parse(fs.readFileSync("./public/OneDark-Pro.json", "utf-8")),
-	light : JSON.parse(fs.readFileSync("./public/OneLight.json", "utf-8")),
+	theme: {
+		dark: JSON.parse(fs.readFileSync("./public/OneDark-Pro.json", "utf-8")),
+		light: JSON.parse(fs.readFileSync("./public/OneLight.json", "utf-8")),
 	},
-keepBackground : false
-  };
-
-
+	keepBackground: false,
+};
 
 // https://astro.build/config
 export default defineConfig({
-	build : {
-		format : "file"
-	},
 	site: "https://example.com",
-	integrations: [mdx(), sitemap(), react(), tailwind() , pagefind()],
-
+	integrations: [mdx(), sitemap(), react(), tailwind(), pagefind()],
 
 	markdown: {
 		syntaxHighlight: false,
-		rehypePlugins : [[rehypePrettyCode, options]]
+		rehypePlugins: [[rehypePrettyCode, options]],
 	},
 });
