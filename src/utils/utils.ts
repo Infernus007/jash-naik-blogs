e97d60e9f8b7be19e3d6f4e72536869ea682ab1d
Infernus007 <100568769+Infernus007@users.noreply.github.com>
2024-10-19 22:04:14 +0530

    feat: blog test 1

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,10 +4,12 @@ export const getImagePromise = (imagePath: string) => {
 	const images = import.meta.glob<{ default: ImageMetadata }>(
 		"/src/assets/*.{jpeg,jpg,png,gif,svg}",
 	);
+	console.log("images", images);
 	if (!images[`/src/assets/${imagePath}`])
 		throw new Error(
 			`"${imagePath}" does not exist in glob: "src/assets/*.{jpeg,jpg,png,gif}"`,
 		);
 
-	return images[`/src/assets/${imagePath}`];
+
+	return images[`/src/assets/${imagePath}`]();
 };
